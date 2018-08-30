26c4179e2ebf79c20d662c48493421de8103c05a
Shailendra Gupta <guptas199614@gmail.com>
2018-08-30 16:46:54 +0530

    Pool Connection

--- utils/db.js
+++ utils/db.js
@@ -7,25 +7,29 @@ const {
   DB_PASSWORD,
 } = require('../config');
 
+const credentials = {
+  host: DB_HOST,
+  user: DB_USERNAME,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+};
+const poolConfig = {
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
+};
+
 let DB;
 if (process.env.DB_SSH_TUNNELED) {
   DB = require('./tunneled-connection'); // eslint-disable-line global-require
 } else {
   DB = new Promise((resolve, reject) => {
-    const connection = mysql.createPool({
-      connectionLimit: 10,
-      host: DB_HOST,
-      user: DB_USERNAME,
-      port: DB_PORT,
-      password: DB_PASSWORD,
-      database: DB_NAME,
-      waitForConnections: true,
-      queueLimit: 0,
+    const connection = mysql.createConnection(credentials);
+    connection.connect((err) => {
+      if (err) reject(err);
+      resolve(mysql.createPool(Object.assign({}, credentials, poolConfig)));
     });
-    // connection.connect((err) => {
-    //   if (err) reject(err);
-    //   resolve(connection);
-    // });
   });
 }
 
